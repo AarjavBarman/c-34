e1a9acaa0026cf9b6a9676fbffbb90843c8601fc
AarjavBarman <74311419+AarjavBarman@users.noreply.github.com>
2021-04-02 21:03:08 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,23 @@
+class Ball {
+ constructor(x,y,r){
+    var options = {
+        "density": 1,
+        "frictionair": .04,
+    }
+    this.body = Bodies.circle(x,y,r,options);
+    this.radius = r;
+    World.add(world,this.body);
+
+ }   
+ display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x,pos.y);
+    rotate(angle);
+    ellipseMode(CENTER);
+    //fill("brown");
+    ellipse(0, 0, this.radius, this.radius);
+    pop();
+  }
+}
